dabb11a8019c086962e02cafc20c0b22a867e3da
Zamzagul <akburanova.zamzagul@gmail.com>
2016-09-23 21:07:00 +0500

    Added SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -22,4 +22,4 @@ helloSpeaker.speak = function(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 window.helloSpeaker = helloSpeaker;
-})(window)
+})(window);
